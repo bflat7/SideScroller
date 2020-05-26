ebac659583420e58cb4c215eb6b1e841d8fe1f19
Patrick <hang.patrick92@gmail.com>
2020-05-26 16:17:21 -0400

    New OnClick implementation

--- SideScroller/Assets/Scripts/Core/GameMenu.cs
+++ SideScroller/Assets/Scripts/Core/GameMenu.cs
@@ -6,15 +6,12 @@ using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour
 {
-    public Button btn_Resume;
-    public Button btn_MainMenu;
-    public Button btn_Quit;
+    //public Button btn_Resume;
+    //public Button btn_MainMenu;
+    //public Button btn_Quit;
 
     void Start()
     {
-        btn_Resume.onClick.AddListener(Resume);
-        btn_MainMenu.onClick.AddListener(() => { Time.timeScale = 1; SceneManager.LoadScene("MainMenu", LoadSceneMode.Single); });
-        btn_Quit.onClick.AddListener(() => { Application.Quit(); });
         this.gameObject.SetActive(false);
     }
 
@@ -26,9 +23,20 @@ public class GameMenu : MonoBehaviour
         }
     }
 
-    private void Resume()
+    public void Resume()
     {
         Time.timeScale = 1;
         this.gameObject.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
